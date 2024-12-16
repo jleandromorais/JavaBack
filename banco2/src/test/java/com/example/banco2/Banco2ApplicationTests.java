c32080a936525a01d777440d89019c37f9991b05
jleandromorais <leandromorais12345@outlook.com>
2024-12-16 11:51:07 -0300

    Adicionando geral

---
+++ banco2/src/test/java/com/example/banco2/Banco2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banco2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Banco2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
