74d467c2bc533a4618a4d7f4ef5aaff99abd45dc
jleandromorais <leandromorais12345@outlook.com>
2024-12-20 12:10:15 -0300

    Versao final do post 20-12-24

--- banco2/src/main/java/com/example/banco2/CARTOES2/CardController.java
+++ banco2/src/main/java/com/example/banco2/CARTOES2/CardController.java
@@ -16,19 +16,25 @@ import java.util.Map;
 public class CardController {
     @Autowired
     private CardService cardService;
+    @Autowired
+    private  CardRepository cardRepository;
 
     @PostMapping
     public List<Card> createCards(@RequestBody Map<String, Object> payload) {
         try {
             Long loggedUserId = Long.valueOf((Integer) payload.get("loggedUserId"));
             List<Map<String, String>> cards = (List<Map<String, String>>) payload.get("cards");
-            return cardService.saveCards(loggedUserId, cards);
+
+            // Salva os novos cartões
+            cardService.saveCards(loggedUserId, cards);
+
+            // Retorna apenas os cartões do usuário logado
+            return cardRepository.findByLoggedUserId(loggedUserId);
         } catch (Exception e) {
             e.printStackTrace();
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao criar cartões", e);
         }
     }
-
     @GetMapping
     public ResponseEntity<List<Card>> getAllCards() {
         List<Card> cards = cardService.getAllCards();
--- banco2/src/main/java/com/example/banco2/CARTOES2/CardRepository.java
+++ banco2/src/main/java/com/example/banco2/CARTOES2/CardRepository.java
@@ -1,8 +1,13 @@
 package com.example.banco2.CARTOES2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
-public interface CardRepository  extends JpaRepository<Card,Long> {
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+    List<Card> findByLoggedUserId(Long loggedUserId);
 }
 
